54fbafbeb80b35f142b20383a538932c4221a38c
Lovethrech <rachealdolapo45@gmail.com>
2026-01-17 11:08:11 +0100

    Inserting the MainHero component into the HomeView.

--- src/components/hero/MainHero.vue
+++ src/components/hero/MainHero.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="main-hero">
-        
+
     </div>
 </template>
+
+<style scoped>
+.main-hero{
+    min-height: 80vh;
+    background: linear-gradient(to bottom, #cae0ff, #ffffff);
+}
+</style>
