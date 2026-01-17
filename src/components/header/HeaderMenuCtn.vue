c08665c7a965aea6961aabf6df6a641919325340
Lovethrech <rachealdolapo45@gmail.com>
2026-01-17 10:14:34 +0100

    Inserting the HeaderMenu into the MainHeader component.

--- src/components/header/HeaderMenuCtn.vue
+++ src/components/header/HeaderMenuCtn.vue
@@ -1,11 +1,16 @@
 <script setup>
+import HeaderMenuDetails from "../data/header-menu-details.json";
 import HeaderMenu from './HeaderMenu.vue';
 </script>
 
 <template>
     <div class="header-menu-ctn-main-ctn">
-        <div class="header-menu-ctn-mini-ctn">
-            <HeaderMenu></HeaderMenu>
+        <div v-for="HeaderMenuDetail in HeaderMenuDetails" :key="HeaderMenuDetail.id" class="header-menu-ctn-mini-ctn">
+            <HeaderMenu 
+                :name="name" 
+                :link="link"
+            >
+            </HeaderMenu>
         </div>
     </div>
 </template>
