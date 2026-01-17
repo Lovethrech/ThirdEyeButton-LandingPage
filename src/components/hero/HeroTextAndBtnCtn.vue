725e3c84cd4eaf871769e24322381c946a381597
Lovethrech <rachealdolapo45@gmail.com>
2026-01-17 13:22:22 +0100

    Inserting the MainHero component into the HomeView.

--- src/components/hero/HeroTextAndBtnCtn.vue
+++ src/components/hero/HeroTextAndBtnCtn.vue
@@ -1,7 +1,17 @@
+<script setup>
+import HeroTextCtn from "./HeroTextCtn.vue";
+</script>
+
 <template>
     <div class="hero-text-and-btn-ctn">
         <div id="hero-text-and-btn-ctn-mini-ctn">
-            
+            <HeroTextCtn></HeroTextCtn>
         </div>
     </div>
 </template>
+
+<style scoped>
+.hero-text-and-btn-ctn{
+    background-color: red;
+}
+</style>
--- src/components/hero/MainHero.vue
+++ src/components/hero/MainHero.vue
@@ -1,6 +1,10 @@
+<script setup>
+import HeroTextAndBtnCtn from './HeroTextAndBtnCtn.vue';
+</script>
+
 <template>
     <div class="main-hero">
-
+        <HeroTextAndBtnCtn></HeroTextAndBtnCtn>
     </div>
 </template>
 
@@ -8,5 +12,6 @@
 .main-hero{
     min-height: 90vh;
     background: linear-gradient(to bottom, #cae0ff, #ffffff);
+    padding: 1vh 4vw;
 }
 </style>
