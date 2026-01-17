a77acb6467c9236f98b946404650afef3f90fddf
Lovethrech <rachealdolapo45@gmail.com>
2026-01-17 10:11:30 +0100

    Inserting the HeaderMenu into the MainHeader component.

--- src/components/header/HeaderMenuCtn.vue
+++ src/components/header/HeaderMenuCtn.vue
@@ -1,11 +1,11 @@
 <script setup>
-    
+import HeaderMenu from './HeaderMenu.vue';
 </script>
 
 <template>
     <div class="header-menu-ctn-main-ctn">
         <div class="header-menu-ctn-mini-ctn">
-
+            <HeaderMenu></HeaderMenu>
         </div>
     </div>
 </template>
