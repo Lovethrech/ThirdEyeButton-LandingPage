d2892022544f9d118dbfc9c540323f3ae0b67761
Lovethrech <rachealdolapo45@gmail.com>
2026-01-17 13:17:42 +0100

    Inserting the MainHero component into the HomeView.

---
+++ src/components/hero/HeroTextCtn.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="hero-text-ctn">
+        <p id="hero-text-des">
+            Empower Your University Examination with ThirdEyeButton
+        </p>
+        <h1 class="hero-text-title">
+            Revolutionizing
+            <br/>
+            examination 
+            <br/>
+            scheduling
+        </h1>
+    </div>
+</template>
