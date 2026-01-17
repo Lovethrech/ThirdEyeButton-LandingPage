6d6d548b47e205194a638cb104533c7e873c3595
Lovethrech <rachealdolapo45@gmail.com>
2026-01-17 10:45:42 +0100

    Inserting the HeaderNav into the MainHeader component.

--- src/components/header/HeaderClientBtn.vue
+++ src/components/header/HeaderClientBtn.vue
@@ -10,12 +10,17 @@
 
 <style scoped>
 #header-client-btn-main-ctn{
-    background-color: aqua;
+    background-color: transparent;
 }
 #header-client-btn{
     background-color: #000000;
     color:#ffffff;
     text-transform: capitalize;
     padding:1vh 2vh;
+}
+#header-client-btn:hover{
+    cursor: pointer;
+    background-color: transparent;
+    color: #000000;
 }
 </style>
