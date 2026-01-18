76b41f406255c45a4e7060d051cea70d6b89ec51
Lovethrech <rachealdolapo45@gmail.com>
2026-01-18 14:09:24 +0100

    ...working on the about component for HomeView.

--- src/components/about/AboutExploreBtn.vue
+++ src/components/about/AboutExploreBtn.vue
@@ -9,21 +9,21 @@
 </template>
 
 <style scoped>
-.about-explore-btn-ctn{
-}
 #about-explore-btn{
     background-color: #000000;
     color: #ffffff;
     font-size:1.6vh;
     font-family: "Roboto Condensed", sans-serif;
     font-weight: 400;
-    padding: 0.2vh 3vh;
+    padding: 1vh 2vh;
     text-transform: capitalize;
     border: 0.2vh solid #000000;
     border-radius: 20vh;
 }
 #about-explore-btn:hover{
     cursor: pointer;
-
+    background-color: #ffffff;
+    color: #282533;
+    transition: all .2s;
 }
 </style>
