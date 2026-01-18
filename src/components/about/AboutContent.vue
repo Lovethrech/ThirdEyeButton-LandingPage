7f8ffe5d47c5d97250e73a1d4253a6f2f7281002
Lovethrech <rachealdolapo45@gmail.com>
2026-01-18 13:10:44 +0100

    ...starting the about component for HomeView.

--- src/components/about/AboutContent.vue
+++ src/components/about/AboutContent.vue
@@ -1,9 +1,11 @@
 <script setup>
 import AboutTitle from './AboutTitle.vue';
+import AboutDesc from './AboutDesc.vue';
 </script>
 
 <template>
     <section class="about-content">
         <AboutTitle></AboutTitle>
+        <AboutDesc></AboutDesc>
     </section>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import MainHero from '@/components/hero/MainHero.vue';
+import AboutContent from '@/components/about/AboutContent.vue';
 </script>
 
 <template>
   <main>
     <MainHero></MainHero>
+    <AboutContent></AboutContent>
   </main>
 </template>
 
