cde261e52e1343ba4ff877607dcc25fae81a500d
Lovethrech <rachealdolapo45@gmail.com>
2026-01-18 13:52:48 +0100

    ...working on the about component for HomeView.

---
+++ src/components/about/AboutExploreBtn.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about-explore-btn-ctn">
+        <RouterLink to="">
+            <button id="about-explore-btn">
+                explore features
+            </button>
+        </RouterLink>
+    </div>
+</template>
+
+<style scoped>
+.about-explore-btn-ctn{
+
+}
+#about-explore-btn{
+    
+}
+</style>
