04a5d6d78e8a47c5d2543b316a90f7eef5092a72
Lovethrech <rachealdolapo45@gmail.com>
2026-01-19 17:30:29 +0100

    ...working on the about component for HomeView.

--- src/components/home/process/ProcessTitleCtn.vue
+++ src/components/home/process/ProcessTitleCtn.vue
@@ -1,7 +1,18 @@
 <template>
     <div class="process-title-ctn">
         <div class="process-title-ctn-mini-ctn">
-            
+            <h3 id="process-title-name">
+                How It Works
+            </h3>
+            <p id="process-title-tag">
+                Seamless Exam Schedulin
+            </p>
+            <p id="process-title-desc">
+                Our process involves advanced scheduling algorithms, automated conflict resolution,
+                and real-time updates to create a sophisticated exam scheduling system. Experience
+                the convenience of managing exam schedules, room assignments, and student
+                allocations effortlessly
+            </p>
         </div>
     </div>
 </template>
