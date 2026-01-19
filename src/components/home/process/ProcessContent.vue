25762a99bf207bd955ce750218f6a7db612816d1
Lovethrech <rachealdolapo45@gmail.com>
2026-01-19 17:32:30 +0100

    ...working on the about component for HomeView.

--- src/components/home/process/ProcessContent.vue
+++ src/components/home/process/ProcessContent.vue
@@ -1,7 +1,11 @@
+<script setup>
+import ProcessTitleCtn from '@/components/home/process/ProcessTitleCtn.vue';
+</script>
+
 <template>
     <div class="process-content">
         <div class="process-content-mini-ctn">
-            
+            <ProcessTitleCtn></ProcessTitleCtn>
         </div>
     </div>
 </template>
