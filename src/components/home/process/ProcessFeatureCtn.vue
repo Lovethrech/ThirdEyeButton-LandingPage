46882fef999a6c1ce2dbb072ad95bb2e26d5191d
Lovethrech <rachealdolapo45@gmail.com>
2026-01-20 11:14:12 +0100

    ...working on the process component for HomeView.

--- src/components/home/process/ProcessFeatureCtn.vue
+++ src/components/home/process/ProcessFeatureCtn.vue
@@ -4,18 +4,22 @@ import ProcessFeature from './ProcessFeature.vue';
 
 <template>
     <div class="process-feature-ctn">
-        <div id="process-feature-mini-ctn">
-            <ProcessFeature></ProcessFeature>
+        <div id="process-feature-main-ctn">
+            <div id="process-feature-mini-ctn">
+                <ProcessFeature></ProcessFeature>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
 .process-feature-ctn{
+    display:flex;
 
 }
-#process-feature-mini-ctn{
+#process-feature-main-ctn{
     width:40%;
+    background-color: red;
     padding: 4vh 0;
     border-bottom: 0.1vh solid #000000;
 }
