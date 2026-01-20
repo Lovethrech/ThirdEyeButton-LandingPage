c67288fc35ca9613561acfe86f2adc4510dd14df
Lovethrech <rachealdolapo45@gmail.com>
2026-01-20 10:58:54 +0100

    ...working on the process component for HomeView.

--- src/components/home/process/ProcessFeature.vue
+++ src/components/home/process/ProcessFeature.vue
@@ -1,3 +1,15 @@
 <template>
-    
+    <div class="process-feature">
+        <div class="process-feature-mini-ctn">
+            <p class="process-feature-sub-title">
+
+            </p>
+            <p class="process-feature-title">
+
+            </p>
+            <p class="process-feature-desc">
+                
+            </p>
+        </div>
+    </div>
 </template>
