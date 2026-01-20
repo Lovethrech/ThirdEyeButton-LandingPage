48f2eb5c2a0b8a0861f258560a939cb4216523ea
Lovethrech <rachealdolapo45@gmail.com>
2026-01-20 11:16:54 +0100

    ...working on the process component for HomeView.

--- src/components/home/process/ProcessFeature.vue
+++ src/components/home/process/ProcessFeature.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="process-feature">
-        <div class="process-feature-mini-ctn">
-            <p class="process-feature-sub-title">
-                User Access
-            </p>
-            <p class="process-feature-title">
-                Registered Users
-            </p>
-            <p class="process-feature-desc">
-                We provide secure access for registered university staff and students, ensuring data privacy and
-                confidentiality. Our system offers role-based permissions to control access and maintain the
-                integrity of exam scheduling information.
-            </p>
-        </div>
+        <p class="process-feature-sub-title">
+            User Access
+        </p>
+        <p class="process-feature-title">
+            Registered Users
+        </p>
+        <p class="process-feature-desc">
+            We provide secure access for registered university staff and students, ensuring data privacy and
+            confidentiality. Our system offers role-based permissions to control access and maintain the
+            integrity of exam scheduling information.
+        </p>
     </div>
 </template>
 
@@ -24,6 +22,6 @@
 
 }
 .process-feature-desc{
-    
+
 }
 </style>
