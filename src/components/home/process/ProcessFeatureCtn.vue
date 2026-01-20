656e7a9608463cc3ddcc294ec2e59c6d99292fa0
Lovethrech <rachealdolapo45@gmail.com>
2026-01-20 10:59:40 +0100

    ...working on the process component for HomeView.

--- src/components/home/process/ProcessFeatureCtn.vue
+++ src/components/home/process/ProcessFeatureCtn.vue
@@ -1,7 +1,11 @@
+<script setup>
+import ProcessFeature from './ProcessFeature.vue';
+</script>
+
 <template>
     <div class="process-feature-ctn">
         <div id="process-feature-mini-ctn">
-            
+            <ProcessFeature></ProcessFeature>
         </div>
     </div>
 </template>
