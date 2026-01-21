439f1ed417a78b180aa61f049b0350d38f396bb5
Lovethrech <rachealdolapo45@gmail.com>
2026-01-21 12:42:29 +0100

    ...working on the review component for HomeView.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 import MainHero from '@/components/home/hero/MainHero.vue';
 import AboutContent from '@/components/home/about/AboutContent.vue';
 import ProcessContent from '@/components/home/process/ProcessContent.vue';
+import ReviewContent from '@/components/home/review/ReviewContent.vue';
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import ProcessContent from '@/components/home/process/ProcessContent.vue';
     <MainHero></MainHero>
     <AboutContent></AboutContent>
     <ProcessContent></ProcessContent>
+    <ReviewContent></ReviewContent>
   </main>
 </template>
 
