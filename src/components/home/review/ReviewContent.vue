4fb6c2daca9ec6e1c8ff062174bf458e2c3f97e2
Lovethrech <rachealdolapo45@gmail.com>
2026-01-21 12:47:27 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewContent.vue
+++ src/components/home/review/ReviewContent.vue
@@ -1,7 +1,11 @@
+<script setup>
+import ProcessTitleCtn from '../process/ProcessTitleCtn.vue';
+</script>
+
 <template>
     <section class="review-content">
         <div id="review-content">
-            
+            <ProcessTitleCtn></ProcessTitleCtn>
         </div>
     </section>
 </template>
