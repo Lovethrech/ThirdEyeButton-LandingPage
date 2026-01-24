085c66c0ae100190fde088671558e0203da6706f
Lovethrech <rachealdolapo45@gmail.com>
2026-01-24 21:49:07 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewCustomer.vue
+++ src/components/home/review/ReviewCustomer.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="review-customer">
-        
+        <p class="review-customer-name">
+
+        </p>
+        <p class="review-customer-comment">
+            
+        </p>
     </div>
 </template>
