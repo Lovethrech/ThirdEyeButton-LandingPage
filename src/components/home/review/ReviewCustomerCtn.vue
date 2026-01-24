792aee35d1b2388cdce1855abc6a3ac7f41dd6f7
Lovethrech <rachealdolapo45@gmail.com>
2026-01-24 21:47:39 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewCustomerCtn.vue
+++ src/components/home/review/ReviewCustomerCtn.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="review-customer-ctn">
-        <div class="review-customer-mini-ctn">
-            
+        <div class="review-customer-main-ctn">
+            <div class="review-customer-mini-ctn">
+                
+            </div>
         </div>
     </div>
 </template>
