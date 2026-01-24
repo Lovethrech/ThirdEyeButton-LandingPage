94a0cd4279de492bc83e559c926dc091aa0ae64c
Lovethrech <rachealdolapo45@gmail.com>
2026-01-24 21:51:43 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewCustomerCtn.vue
+++ src/components/home/review/ReviewCustomerCtn.vue
@@ -1,8 +1,12 @@
+<script setup>
+import ReviewCustomer from './ReviewCustomer.vue';
+</script>
+
 <template>
     <div class="review-customer-ctn">
         <div class="review-customer-main-ctn">
             <div class="review-customer-mini-ctn">
-                
+                <ReviewCustomer></ReviewCustomer>
             </div>
         </div>
     </div>
