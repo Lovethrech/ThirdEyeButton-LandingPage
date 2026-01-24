0021410ed0eef2b6a2d161ca5517d78b21ab4610
Lovethrech <rachealdolapo45@gmail.com>
2026-01-24 21:54:39 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewCustomer.vue
+++ src/components/home/review/ReviewCustomer.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="review-customer">
         <p class="review-customer-name">
-
+            <q>
+                Olaonipekun Dolapo Raxhael
+        </q>
+            
         </p>
         <p class="review-customer-comment">
-            
+
         </p>
     </div>
 </template>
