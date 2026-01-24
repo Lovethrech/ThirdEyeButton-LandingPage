0a71ec81fb8c1a8da4a5cfd56c1f8ce4b5dc5aee
Lovethrech <rachealdolapo45@gmail.com>
2026-01-24 22:02:46 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewCustomerCtn.vue
+++ src/components/home/review/ReviewCustomerCtn.vue
@@ -1,12 +1,16 @@
 <script setup>
-import reviewCustomerDetails from "@/data/review-custmer-details.json";
+import reviewCustomerDetails from "@/data/review-customer-details.json";
 import ReviewCustomer from './ReviewCustomer.vue';
 </script>
 
 <template>
     <div class="review-customer-ctn">
         <div class="review-customer-main-ctn">
-            <div class="review-customer-mini-ctn">
+            <div 
+                v-for="reviewCustomerDetail in reviewCustomerDetails"
+                :key="reviewCustomerDetail.id"
+                class="review-customer-mini-ctn"
+            >
                 <ReviewCustomer></ReviewCustomer>
             </div>
         </div>
