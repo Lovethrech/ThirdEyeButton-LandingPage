2505dd4d0304870a54d774a7a5fc0ffebb14bf5c
Lovethrech <rachealdolapo45@gmail.com>
2026-01-24 21:29:31 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewContent.vue
+++ src/components/home/review/ReviewContent.vue
@@ -7,9 +7,9 @@ import ProcessTitleCtn from '../process/ProcessTitleCtn.vue';
     <section class="review-content">
         <div id="review-content-mini-ctn">
             <ProcessTitleCtn
-                :title="processAndReviewDetails[0].title"
-                :tag="processAndReviewDetails[0].tag"
-                :desc="processAndReviewDetails[0].desc"
+                :title="processAndReviewDetails[1].title"
+                :tag="processAndReviewDetails[1].tag"
+                :desc="processAndReviewDetails[1].desc"
             >
             </ProcessTitleCtn>
         </div>
