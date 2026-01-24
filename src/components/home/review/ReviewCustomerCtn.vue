968dea09db0539da726ce8f9f4ad7e784a582a62
Lovethrech <rachealdolapo45@gmail.com>
2026-01-24 22:28:14 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewCustomerCtn.vue
+++ src/components/home/review/ReviewCustomerCtn.vue
@@ -36,10 +36,10 @@ import ReviewCustomer from './ReviewCustomer.vue';
 
 @media screen and (max-width:990px) and (min-width:346px){
     .review-customer-ctn{
-
+        width: 300vw;
     }
     .review-customer-main-ctn{
-        width: 300vw;
+        width: 100%;
         flex-direction: row;
         justify-content: space-between;
     }
