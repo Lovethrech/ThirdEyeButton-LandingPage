19a595e9f86d415291a9bd35677b06a79b036fa7
Lovethrech <rachealdolapo45@gmail.com>
2026-01-25 23:55:33 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewCustomer.vue
+++ src/components/home/review/ReviewCustomer.vue
@@ -34,7 +34,7 @@ const {name, desc}=defineProps(['name', 'desc']);
 @media screen and (max-width: 990px){
     .review-customer-name{
         width:50%;
-        height: 
+        height: 1
     }
     .review-customer-comment-ctn{
         text-align: justify;
