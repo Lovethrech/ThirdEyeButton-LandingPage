e2622f94f52eca6112775805bfce1527a23a0338
Lovethrech <rachealdolapo45@gmail.com>
2026-01-28 19:07:07 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewCustomer.vue
+++ src/components/home/review/ReviewCustomer.vue
@@ -13,7 +13,8 @@ const {name, desc}=defineProps(['name', 'desc']);
         >
             {{ name }}
         </p>
-        <div class="review-customer-comment-ctn" v-html="desc"></div>
+        <div 
+        class="review-customer-comment-ctn" v-html="desc"></div>
     </div>
 </template>
 
