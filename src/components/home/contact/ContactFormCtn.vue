30ecc0a45c31da7c39d07fb47e509bdf3513f3e5
Lovethrech <rachealdolapo45@gmail.com>
2026-01-30 23:26:27 +0100

    ...working on the review component for HomeView.

--- src/components/home/contact/ContactFormCtn.vue
+++ src/components/home/contact/ContactFormCtn.vue
@@ -1,10 +1,11 @@
 <script setup>
+import contactLabelAndInputFormats from "@/data/contact-label-and-input-formats.json";
 import ContactFormLabelAndInput from './ContactFormLabelAndInput.vue';
 </script>
 
 <template>
     <div class="contact-form-ctn">
-        <div class="contact-form-mini-ctn">
+        <div class="contact-form-mini-ctn" v-for="contactLabelAndInputFormat in contactLabelAndInputFormats" :key="contactLabelAndInputFormat.id">
             <ContactFormLabelAndInput></ContactFormLabelAndInput>
         </div>
     </div>
