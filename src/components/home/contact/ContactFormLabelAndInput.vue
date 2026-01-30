19c0a2bf156250a5ab9fb38a1083bfc3dc3dabc6
Lovethrech <rachealdolapo45@gmail.com>
2026-01-30 20:28:17 +0100

    ...working on the review component for HomeView.

--- src/components/home/contact/ContactFormLabelAndInput.vue
+++ src/components/home/contact/ContactFormLabelAndInput.vue
@@ -1,3 +1,6 @@
 <template>
-    
+    <div class="contact-form-label-and-input-ctn">
+        <label for="" class="contact-form-label-and-input-ctn-label"></label>
+        <input type="text" class="contact-form-label-and-input-ctn-input">
+    </div>
 </template>
