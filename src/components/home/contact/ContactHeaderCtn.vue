9206c3c2c5d44d6f46c7735b0b5d325ea9fe0671
Lovethrech <rachealdolapo45@gmail.com>
2026-01-30 20:04:05 +0100

    ...working on the review component for HomeView.

--- src/components/home/contact/ContactHeaderCtn.vue
+++ src/components/home/contact/ContactHeaderCtn.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="contact-header-ctn">
         <h3 class="contact-header-title">
-            
+
         </h3>
+        <p class="contact-header-subtitle">
+
+        </p>
+        <p class="contact-header-desc">
+            
+        </p>
     </div>
 </template>
