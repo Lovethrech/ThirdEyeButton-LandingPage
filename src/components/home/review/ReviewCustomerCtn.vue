466ed01742c1fe054e4694ddcdaaeae7420c0034
Lovethrech <rachealdolapo45@gmail.com>
2026-01-30 19:12:35 +0100

    ...working on the review component for HomeView.

--- src/components/home/review/ReviewCustomerCtn.vue
+++ src/components/home/review/ReviewCustomerCtn.vue
@@ -7,7 +7,8 @@ import ReviewCustomer from './ReviewCustomer.vue';
     <div 
         class="review-customer-ctn"
     >
-        <div class="review-customer-main-ctn">
+        <div 
+        class="review-customer-main-ctn">
             <div 
                 v-for="reviewCustomerDetail in reviewCustomerDetails"
                 :key="reviewCustomerDetail.id"
