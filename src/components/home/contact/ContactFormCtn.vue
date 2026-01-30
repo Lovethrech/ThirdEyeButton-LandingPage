61912fa3cfa8b6666bb730ef41cbd7b79101b8c7
Lovethrech <rachealdolapo45@gmail.com>
2026-01-30 23:13:28 +0100

    ...working on the review component for HomeView.

--- src/components/home/contact/ContactFormCtn.vue
+++ src/components/home/contact/ContactFormCtn.vue
@@ -1,7 +1,11 @@
+<script setup>
+import ContactFormLabelAndInput from './ContactFormLabelAndInput.vue';
+</script>
+
 <template>
     <div class="contact-form-ctn">
         <div class="contact-form-mini-ctn">
-            
+            <ContactFormLabelAndInput></ContactFormLabelAndInput>
         </div>
     </div>
 </template>
