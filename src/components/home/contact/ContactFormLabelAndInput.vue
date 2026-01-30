6f1fb00b216d39573da8669f7f71a0d9b93e2fef
Lovethrech <rachealdolapo45@gmail.com>
2026-01-30 23:20:17 +0100

    ...working on the review component for HomeView.

--- src/components/home/contact/ContactFormLabelAndInput.vue
+++ src/components/home/contact/ContactFormLabelAndInput.vue
@@ -8,5 +8,18 @@
 </template>
 
 <style scoped>
-
+.contact-form-label-and-input-ctn{
+    display: flex;
+    flex-direction: column;
+}
+.contact-form-label-and-input-ctn-label{
+    font-size: 1.8vh;
+}
+.contact-form-label-and-input-ctn-input{
+    margin: 1vh 0;
+    border: 0;
+    border-bottom: 0.2vh solid #000000;
+    padding: 1vh 1vw;
+    font-size: 1.8vh;
+}
 </style>
