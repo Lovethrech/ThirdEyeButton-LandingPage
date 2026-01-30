a51a02f9ec3701a87e8842a41d4f3926ad98b182
Lovethrech <rachealdolapo45@gmail.com>
2026-01-31 00:12:34 +0100

    ...working on the review component for HomeView.

--- src/components/home/contact/ContactFormBtn.vue
+++ src/components/home/contact/ContactFormBtn.vue
@@ -1,7 +1,16 @@
 <template>
-    <div class="contact-form-btn-ctn">
-        <a href="" target="_blank" rel="noopener noreferrer" id="contact-form-btn-link">
-            <button class="contact-form-btn">
+    <div 
+        class="contact-form-btn-ctn"
+    >
+        <a 
+            href="" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            id="contact-form-btn-link"
+        >
+            <button 
+                class="contact-form-btn"
+            >
                 Submit
             </button>
         </a>
