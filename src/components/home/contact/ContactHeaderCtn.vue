515cefb71f21d946dfc52bb343384301735bf984
Lovethrech <rachealdolapo45@gmail.com>
2026-01-30 20:05:11 +0100

    ...working on the review component for HomeView.

--- src/components/home/contact/ContactHeaderCtn.vue
+++ src/components/home/contact/ContactHeaderCtn.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="contact-header-ctn">
         <h3 class="contact-header-title">
-
+            Contact Us
         </h3>
         <p class="contact-header-subtitle">
-
+            Reach Out to ThirdEyeButton
         </p>
         <p class="contact-header-desc">
-            
+            Have questions or interested in learning more about our
+            default website interface for exam scheduling? Contact us
+            today to explore how ThirdEyeButton can elevate your
+            university's exam management
         </p>
     </div>
 </template>
