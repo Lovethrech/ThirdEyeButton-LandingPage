00a0f2709140a0a712e7ba9bf6ab997427c7255c
Lovethrech <rachealdolapo45@gmail.com>
2026-01-30 23:41:07 +0100

    ...working on the review component for HomeView.

---
+++ src/components/home/contact/ContactFormBtn.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="contact-form-btn">
+        <a href="http://" target="_blank" rel="noopener noreferrer" id="contact-form-btn-link">
+            
+        </a>
+    </div>
+</template>
