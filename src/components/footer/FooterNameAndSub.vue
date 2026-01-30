fd37971c4f9818cbbd333a8b8d69198cb6d5ed01
Lovethrech <rachealdolapo45@gmail.com>
2026-01-31 00:24:50 +0100

    ...working on the review component for HomeView.

--- src/components/footer/FooterNameAndSub.vue
+++ src/components/footer/FooterNameAndSub.vue
@@ -1,5 +1,9 @@
+<script setup>
+import FooterName from './FooterName.vue';
+</script>
+
 <template>
     <div class="footer-name-and-sub">
-        
+        <FooterName></FooterName>
     </div>
 </template>
