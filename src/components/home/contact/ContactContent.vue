86150cd625dbf8d3849c0cb964ce4eaaa6f064b0
Lovethrech <rachealdolapo45@gmail.com>
2026-01-30 19:57:07 +0100

    ...working on the review component for HomeView.

--- src/components/home/contact/ContactContent.vue
+++ src/components/home/contact/ContactContent.vue
@@ -1,5 +1,13 @@
 <template>
     <section class="contact-content">
-        
+
     </section>
 </template>
+
+<style scoped>
+.contact-content{
+    width: 1905;
+    min-height: 100vh;
+    background-color: #ffffff;
+}
+</style>
