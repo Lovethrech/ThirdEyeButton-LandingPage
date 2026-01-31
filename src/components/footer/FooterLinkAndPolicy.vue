c06c741d2877f6723cbf524552be6767e980c5c5
Lovethrech <rachealdolapo45@gmail.com>
2026-01-31 23:12:02 +0100

    ...fine tuning the code.

--- src/components/footer/FooterLinkAndPolicy.vue
+++ src/components/footer/FooterLinkAndPolicy.vue
@@ -4,8 +4,12 @@ import FooterPolicyCtn from './FooterPolicyCtn.vue';
 </script>
 
 <template>
-    <div class="footer-link-and-policy">
-        <div class="footer-link-and-policy-mini-ctn">
+    <div 
+        class="footer-link-and-policy"
+    >
+        <div 
+            class="footer-link-and-policy-mini-ctn"
+        >
             <FooterTelAddrIcon></FooterTelAddrIcon>
             <FooterPolicyCtn></FooterPolicyCtn>
         </div>
