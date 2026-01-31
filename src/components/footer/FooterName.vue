82a867f65232a9e3d6e166a96290ed0bdc242e87
Lovethrech <rachealdolapo45@gmail.com>
2026-01-31 23:12:22 +0100

    ...fine tuning the code.

--- src/components/footer/FooterName.vue
+++ src/components/footer/FooterName.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="footer-name">
-        <p class="footer-name-and-sub-name">
+    <div 
+        class="footer-name"
+    >
+        <p 
+            class="footer-name-and-sub-name"
+        >
             Dr. Ezike
         </p>
     </div>
