458db3e21ad99741bca20add837173acd3c3f4be
Lovethrech <rachealdolapo45@gmail.com>
2026-01-31 01:01:52 +0100

    ...working on the review component for HomeView.

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,11 +1,13 @@
 <script setup>
 import FooterNameAndSub from './footer/FooterNameAndSub.vue';
+import FooterLinkAndPolicy from './footer/FooterLinkAndPolicy.vue';
 </script>
 
 <template>
     <footer>
         <div class="footer-main-ctn">
             <FooterNameAndSub></FooterNameAndSub>
+            <FooterLinkAndPolicy></FooterLinkAndPolicy>
         </div>
     </footer>
 </template>
