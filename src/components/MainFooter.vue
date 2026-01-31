d2e5caf8b65bb010ee8571d366155808b6186fe8
Lovethrech <rachealdolapo45@gmail.com>
2026-01-31 23:11:25 +0100

    ...fine tuning the code.

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -5,7 +5,9 @@ import FooterLinkAndPolicy from './footer/FooterLinkAndPolicy.vue';
 
 <template>
     <footer>
-        <div    class="footer-main-ctn">
+        <div 
+            class="footer-main-ctn"
+        >
             <FooterNameAndSub></FooterNameAndSub>
             <FooterLinkAndPolicy></FooterLinkAndPolicy>
         </div>
