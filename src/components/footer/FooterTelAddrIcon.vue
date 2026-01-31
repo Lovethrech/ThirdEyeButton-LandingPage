f3fa4a5842aa77c9832bfbfe6bcf323e03dcd75d
Lovethrech <rachealdolapo45@gmail.com>
2026-01-31 23:19:04 +0100

    ...fine tuning the code.

--- src/components/footer/FooterTelAddrIcon.vue
+++ src/components/footer/FooterTelAddrIcon.vue
@@ -1,21 +1,38 @@
 <template>
-    <div class="footer-tel-address-and-icon-ctn">
-        <div class="footer-tel-address-and-icon-ctn-tel-and-link">
-            <p class="footer-tel">
+    <div 
+        class="footer-tel-address-and-icon-ctn"
+    >
+        <div 
+            class="footer-tel-address-and-icon-ctn-tel-and-link"
+        >
+            <p 
+                class="footer-tel"
+            >
                 123-456-7890
             </p>
-            <p class="footer-link">
+            <p 
+                class="footer-link"
+            >
                 info@mysite.com
             </p>
         </div>
-        <div class="footer-address-and-icon-ctn">
-            <p class="footer-address-name">
+        <div 
+            class="footer-address-and-icon-ctn"
+        >
+            <p 
+                class="footer-address-name"
+            >
                 Idiroko, Benji Village
                 <br/>
                 NG, 100217
             </p>
-            <div class="footer-icon-ctn">
-                <div class="footer-icon"></div>
+            <div 
+                class="footer-icon-ctn"
+            >
+                <div 
+                    class="footer-icon"
+                >
+                </div>
             </div>
         </div>
         
