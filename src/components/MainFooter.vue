f8d985411969426b82405e5745d8523de41c5d2c
Lovethrech <rachealdolapo45@gmail.com>
2026-01-31 21:30:20 +0100

    ...working on the review component for HomeView.

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -24,6 +24,7 @@ footer{
 @media screen and (max-width:990px) {
     footer{
         border-top: 0.5vh double #000000;
+        background: linear-gradient(to bottom, #ffffff, #cae0ff);
     }
     .footer-main-ctn{
         flex-direction: column;
