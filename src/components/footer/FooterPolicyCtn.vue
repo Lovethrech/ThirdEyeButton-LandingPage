d2ae4cc6cdcbc2db415e90a663e0f4293e65f3bf
Lovethrech <rachealdolapo45@gmail.com>
2026-01-31 01:26:01 +0100

    ...working on the review component for HomeView.

---
+++ src/components/footer/FooterPolicyCtn.vue
@@ -0,0 +1,26 @@
+<script setup>
+const policies=[
+    {
+        "id":"1",
+        "name":"Terms and Conditions"
+    },
+    {
+        "id":"2",
+        "name":"privacy policy"
+    },
+    {
+        "id":"3",
+        "name":"refund policy"
+    },
+    {
+        "id":"4",
+        "name":"accessibility statement"
+    }
+]
+</script>
+
+<template>
+    <div class="footer-policy-ctn">
+
+    </div>
+</template>
