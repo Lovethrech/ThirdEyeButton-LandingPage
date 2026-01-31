d81f3e6fc7ddd574c4c49821f88a96a3c6e0a0d6
Lovethrech <rachealdolapo45@gmail.com>
2026-01-31 01:08:04 +0100

    ...working on the review component for HomeView.

--- src/components/footer/FooterLinkAndPolicy.vue
+++ src/components/footer/FooterLinkAndPolicy.vue
@@ -1,5 +1,11 @@
+<script setup>
+import FooterTelAddrIcon from './FooterTelAddrIcon.vue';
+</script>
+
 <template>
     <div class="footer-link-and-policy">
-        
+        <div class="footer-link-and-policy-mini-ctn">
+            <FooterTelAddrIcon></FooterTelAddrIcon>
+        </div>
     </div>
 </template>
--- src/components/footer/FooterTelAddrIcon.vue
+++ src/components/footer/FooterTelAddrIcon.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="footer-tel-address-and-icon-ctn">
-        
+        <p class="footer-tel">
+            123-456-7890
+        </p>
     </div>
 </template>
