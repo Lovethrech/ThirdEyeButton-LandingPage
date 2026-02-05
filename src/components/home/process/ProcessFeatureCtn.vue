126e82d56677e3b4b34bf1ec4bdc61b77d7e3c09
Lovethrech <rachealdolapo45@gmail.com>
2026-02-05 11:15:43 +0100

    ...working on Readme doc.

--- src/components/home/process/ProcessFeatureCtn.vue
+++ src/components/home/process/ProcessFeatureCtn.vue
@@ -39,7 +39,7 @@ import ProcessFeatureIcon from './ProcessFeatureIcon.vue';
 .process-feature-ctn{
     justify-content: right;
     margin-bottom: 10vh;
-    bo
+    bor
 }
 #process-feature-main-ctn{
     width:44%;    
